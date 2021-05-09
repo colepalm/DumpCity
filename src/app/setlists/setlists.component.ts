d17c4a35ee4920b0e53cf5257187cb828a60fbc7
colepalm <palm.cole@gmail.com>
2021-05-08 21:18:17 -0600

    introduce pagination

--- src/app/setlists/setlists.component.ts
+++ src/app/setlists/setlists.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { PageEvent } from '@angular/material/paginator';
+import { QueryRef } from 'apollo-angular';
+import { Subscription } from 'rxjs';
 
 import { AllShowsService } from '../shared/services/show.service';
 
@@ -8,17 +11,34 @@ import { AllShowsService } from '../shared/services/show.service';
   templateUrl: './setlists.component.html',
   styleUrls: ['./setlists.component.scss']
 })
-export class SetlistsComponent implements OnInit {
+export class SetlistsComponent implements OnInit, OnDestroy {
   shows: any;
+
+  take = 10;
+  skip = 0;
+
+  showsQuery: QueryRef<any> = this.allShowService.watch({ take: this.take, skip: this.skip });
+  showsSubscription: Subscription;
 
   constructor(private allShowService: AllShowsService) { }
 
   ngOnInit() {
-    this.allShowService.watch({ take: 20, skip: 100})
+    this.showsSubscription = this.showsQuery
       .valueChanges
-      .pipe(
-        map((result: any) => this.shows = result.data.shows)
-      )
+      .pipe(map((result: any) => this.shows = result.data.shows))
       .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.showsSubscription.unsubscribe();
+  }
+
+  refreshList(event: PageEvent) {
+    this.take = event.pageSize;
+    this.skip = event.pageIndex * this.take;
+
+    this.showsQuery.fetchMore({
+      variables: { take: this.take, skip: this.skip }
+    });
   }
 }
